fded2b7608ad6613543d3f8f7dd25ac5d43753a6
Nas Villanueva <nas@infoteria.com>
2018-10-16 00:03:17 +0900

    Configure prismjs line numbers

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,3 @@
-require("prismjs/themes/prism-tomorrow.css")
+require('prismjs/themes/prism-tomorrow.css')
+require('prismjs/plugins/line-numbers/prism-line-numbers.css')
+require('./src/assets/line-number-ext.less')
