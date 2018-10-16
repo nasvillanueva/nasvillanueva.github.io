ed8258ec77b5639981804cf8229a39a85f1b7605
Nas Villanueva <nas@infoteria.com>
2018-10-16 11:34:09 +0900

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0f70c01a778ed0ca3793.js"
+    "url": "webpack-runtime-eab44360a9480c4f4b9b.js"
   },
   {
     "url": "app.aeed3c949efaeb1e8de3.css"
@@ -39,11 +39,11 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "7675f99824178112cc6414303d12f191"
+    "revision": "000e2084937f33e26a383f4187321add"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e53e5944956030987b1ba4d1c3a66ba3"
+    "revision": "b5ce1c428aef5ac5a638de8c0100fe6c"
   },
   {
     "url": "0.e9035a2f32676513aa77.css"
