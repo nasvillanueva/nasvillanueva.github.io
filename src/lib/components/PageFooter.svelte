68f367e50af5e5f6cdd5b670d6c5a90c23673751
Nas Villanueva <jonas.villanueva@restar-inc.com>
2023-03-05 11:22:26 +0900

    Make footer links accessible

--- src/lib/components/PageFooter.svelte
+++ src/lib/components/PageFooter.svelte
@@ -1,29 +1,31 @@
+<script lang="ts">
+  const links = [
+    {
+      href: 'https://instagram.com/nandemonas',
+      iconClass: 'fa-instagram',
+    },
+    {
+      href: 'https://youtube.com/@nasvillanueva',
+      iconClass: 'fa-youtube',
+    },
+    {
+      href: 'https://github.com/nasvillanueva',
+      iconClass: 'fa-github',
+    },
+    {
+      href: 'https://linkedin.com/in/nasvillanueva',
+      iconClass: 'fa-linkedin-in',
+    },
+  ] as const;
+</script>
+
 <footer class="flex items-center justify-between bg-zinc-800 p-14 text-xl text-white max-md:p-8">
   <div class="links">
-    <a href="https://instagram.com/nandemonas" class="custom-link"
-      ><i class="fa-brands fa-instagram" /></a
-    >
-    <a href="https://youtube.com/@nasvillanueva" class="custom-link"
-      ><i class="fa-brands fa-youtube" /></a
-    >
-    <a href="https://github.com/nasvillanueva" class="custom-link"
-      ><i class="fa-brands fa-github" /></a
-    >
-    <a href="https://linkedin.com/in/nasvillanueva" class="custom-link"
-      ><i class="fa-brands fa-linkedin-in" /></a
-    >
+    {#each links as { href, iconClass }, index}
+      <a {href} class="custom-link hover:text-nxs-red" class:ml-3={index !== 0} aria-label={href}
+        ><i class={`fa-brands ${iconClass}`} /></a
+      >
+    {/each}
   </div>
   <div class="text-xs">&#169; {new Date().getFullYear()} Jonas Villanueva</div>
 </footer>
-
-<style lang="scss">
-  .links {
-    > a {
-      @apply hover:text-nxs-red;
-
-      &:not(:first-child) {
-        @apply ml-1;
-      }
-    }
-  }
-</style>
