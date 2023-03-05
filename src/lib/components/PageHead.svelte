084f75aa2dd5da79a650e21955bd50725f3574a3
Nas Villanueva <jonas.villanueva@restar-inc.com>
2023-03-05 11:37:38 +0900

    Update meta tags

--- src/lib/components/PageHead.svelte
+++ src/lib/components/PageHead.svelte
@@ -2,6 +2,7 @@
   export let title: string | null = null;
 
   const siteTitle = 'nandemonas';
+  const description = 'Personal website of Nas Villanueva.';
   const formattedTitle = title == null ? siteTitle : `${siteTitle} | ${title}`;
 </script>
 
@@ -9,7 +10,6 @@
   <title>{formattedTitle}</title>
 
   <meta property="og:site_name" content={siteTitle} />
-  {#if title != null}
-    <meta property="og:title" content={title} />
-  {/if}
+  <meta name="title" property="og:title" content={title ?? siteTitle} />
+  <meta name="description" property="og:description" content={description} />
 </svelte:head>
