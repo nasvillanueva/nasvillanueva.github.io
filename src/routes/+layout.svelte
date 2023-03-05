ef5efdbcb5da8d7637a79e2017c3822b4ade78bb
Nas Villanueva <jonas.villanueva@restar-inc.com>
2023-03-05 11:23:50 +0900

    Rename PageHeader to PageNav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,12 +7,12 @@
   import '@fortawesome/fontawesome-free/js/fontawesome';
 
   import { page } from '$app/stores';
-  import PageHeader from '$lib/components/PageHeader.svelte';
+  import PageNav from '$lib/components/PageNav.svelte';
   import PageFooter from '$lib/components/PageFooter.svelte';
 </script>
 
 <div class={`flex h-full flex-col ${$page.url.pathname === '/' ? 'pt-0' : 'pt-52'}`}>
-  <PageHeader />
+  <PageNav />
   <main class="mb-20 grow">
     <slot />
   </main>
